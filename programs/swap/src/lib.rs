5358ff60f8432e330d4f1851de37dffdd017a6c1
sardarahmedkhan505 <sardarahmedkhan59@gmail.com>
2024-10-24 11:25:19 +0500

    intial setup

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("C6E82rhkqDHB3PsccYffy9A3HyU9sBnT6gkWTNcrERBT");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
